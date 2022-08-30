8144571661811ebd093963b464cecc1983b93d15
gustaavoevangelista <gustavo.eng.henrique@gmail.com>
2022-08-30 23:28:32 +0100

    Trying to solve Unexpected end of JSON input

--- js/index.js
+++ js/index.js
@@ -14,22 +14,28 @@ let search = document.getElementById('search').value
 let btn_search = addEventListener("click", function searchHouses(req,response) {
 
     let data = {
-        state_code: state_code,
-        city: city,
-        offset: offset,
-        limit: limit
+        state_code: "FL",
+        city: search,
+        offset: 0,
+        limit: 20
     }
 
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': '4af856f0d1mshc1d8102f0086981p10e823jsnc6ac426683f6',
-            'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
+            'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com', 
+            'Content-type': 'application/json;charset=UTF-8',
+            "Access-Control-Allow-Origin" : "*", 
+            "Access-Control-Allow-Credentials" : true 
         },
+        
+        /* params: {data} */
 
     };
     
-    fetch('https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=NY&city=New%20York%20City&offset=0&limit=200&sort=relevance', options)
+    fetch('https://realty-in-us.p.rapidapi.com/properties/list-for-sale', options)
+        
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));
